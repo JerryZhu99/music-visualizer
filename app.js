d160b6d432799956e8bd694615539ab744a3064c
JerryZ <jerryzhu99@gmail.com>
2017-02-06 20:04:36 -0800

    added jquery, changed to frequency graph

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-function start(){
-    console.log("started");
-    chrome.desktopCapture.chooseDesktopMedia(["screen","window"], onApproved)
+
+function resize(){
+    $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
+}
+$(document).ready(function(){
+    resize();
+    $(window).on("resize", function(){
+        resize();
+    });
+});
+
+var recording = false;
+var local_stream = null;
+function toggle(){
+    if(!recording){
+        recording = true;
+        console.log("started");
+        chrome.desktopCapture.chooseDesktopMedia(["screen","audio"], onApproved);
+    }else{
+        recording = false;
+        if(local_stream)
+            local_stream.stop();
+        local_stream=null;
+    }
 }
 
-function onApproved(desktop_id){
+function onApproved(streamId){
     navigator.webkitGetUserMedia({
-        audio: false,
-        video: {
+        audio: {
+            mandatory: {
+                chromeMediaSource: 'system',
+                chromeMediaSourceId: streamId,
+            }
+        },
+        video:  {
             mandatory: {
                 chromeMediaSource: 'desktop',
-                chromeMediaSourceId: desktop_id,
+                chromeMediaSourceId: streamId,
                 minWidth: 1280,
                 maxWidth: 1280,
                 minHeight: 720,
@@ -20,11 +46,60 @@ function onApproved(desktop_id){
 
     function getStream(stream) {
         local_stream = stream;
-        document.querySelector('video').src = URL.createObjectURL(stream);
+        //    var a = document.querySelector('video')
+        //    a.src = URL.createObjectURL(stream);
+        //    a.play();
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        var analyser = audioCtx.createAnalyser();
+        source = audioCtx.createMediaStreamSource(stream);
+        source.connect(analyser);
+
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        analyser.getByteFrequencyData(dataArray);
+
+        // Get a canvas defined with ID "oscilloscope"
+        var canvas = document.getElementById("canvas");
+        var canvasCtx = canvas.getContext("2d");
+
+        // draw an oscilloscope of the current audio source
+
+        function draw() {
+            var WIDTH= canvas.width;
+            var HEIGHT = canvas.height;
+            console.log("draw");
+            drawVisual = requestAnimationFrame(draw);
+
+            analyser.getByteFrequencyData(dataArray);
+
+            canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+            canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+            var barWidth = (WIDTH / bufferLength) * 7.5;
+            var barHeight;
+            var x = 0;
+
+            for(var i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i]/256.0 * HEIGHT;
+
+                canvasCtx.fillStyle = 'rgb(0,0,' + (dataArray[i]) + ')';
+                canvasCtx.fillRect(x,HEIGHT,barWidth,-barHeight);
+
+                x += barWidth;
+            }
+        };
+
+        draw();
+
     }
     function getError(error){
         console.log(error)
     }
 
 }
-document.querySelector('button').addEventListener("click",function(e){start()});
+document.querySelector('button').addEventListener("click",function(e){toggle()});
+document.querySelector('canvas').addEventListener('click', function() {
+    chrome.app.window.current().fullscreen();
+});
+toggle();
